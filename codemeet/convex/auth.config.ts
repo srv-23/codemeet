89d9187bb70a788dd363882a2541ce4c0d63ad8e
srv-23 <srvchourasia10@gmail.com>
2025-04-16 13:13:19 +0530

    Remove codemeet folder from repository

--- codemeet/convex/auth.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    providers: [
-        {
-            domain: "https://humane-bulldog-7.clerk.accounts.dev",
-            applicationID: "convex",
-        },
-    ]
-};
--- codemeet/src/app/(root)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import StreamClientProvider from "@/components/providers/StreamClientProvider";
-
-function Layout({ children }: { children: React.ReactNode }) {
-  return <StreamClientProvider>{children}</StreamClientProvider>;
-}
-export default Layout;
